ce6912b055f5170337e8c7e96ad11dc72a2125a8
Eduardo Tavares <eduardot.santosdev@gmail.com>
2024-12-22 19:48:39 -0300

    feat: adiciona findById

--- src/domain/application/repositories/participants-repository.ts
+++ src/domain/application/repositories/participants-repository.ts
@@ -1,6 +1,7 @@
 import { Participant } from '@/domain/enterprise/entities/participant'
 
 export abstract class ParticipantsRepository {
+  abstract findById(id: string): Promise<Participant | null>
   abstract findByEmail(email: string): Promise<Participant | null>
   abstract create(participant: Participant): Promise<void>
 }
