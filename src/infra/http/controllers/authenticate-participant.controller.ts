e73693eebbadf6639c71f18396ca8b5287c4adc9
Eduardo Tavares <eduardot.santosdev@gmail.com>
2024-12-22 13:43:00 -0300

    feat: cria controller de autenticação e registra no http module

---
+++ src/infra/http/controllers/authenticate-participant.controller.ts
@@ -0,0 +1,60 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common'
+import { z } from 'zod'
+
+import { AuthenticateParticipantUseCase } from '@/domain/application/use-cases/authenticate-participant'
+import { WrongCredentialsError } from '@/domain/application/use-cases/errors/wrong-credentials-error'
+import { Public } from '@/infra/auth/public'
+
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+
+const authenticateParticipantBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type AuthenticateParticipantBodySchema = z.infer<
+  typeof authenticateParticipantBodySchema
+>
+
+@Controller('/sessions')
+@Public()
+export class AuthenticateParticipantController {
+  constructor(
+    private authenticateParticipant: AuthenticateParticipantUseCase,
+  ) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(authenticateParticipantBodySchema))
+  async handle(@Body() body: AuthenticateParticipantBodySchema) {
+    const { email, password } = body
+
+    const result = await this.authenticateParticipant.execute({
+      email,
+      password,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+
+      switch (error.constructor) {
+        case WrongCredentialsError:
+          throw new UnauthorizedException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
+    }
+
+    const { accessToken } = result.value
+
+    return {
+      access_token: accessToken,
+    }
+  }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common'
 
+import { AuthenticateParticipantUseCase } from '@/domain/application/use-cases/authenticate-participant'
 import { RegisterParticipantUseCase } from '@/domain/application/use-cases/register-participant'
 import { CryptographyModule } from '@/infra/cryptography/cryptography.module'
 import { DatabaseModule } from '@/infra/database/database.module'
+import { AuthenticateParticipantController } from '@/infra/http/controllers/authenticate-participant.controller'
+import { RegisterParticipantController } from '@/infra/http/controllers/register-participant.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
-  controllers: [ReadableByteStreamController],
-  providers: [RegisterParticipantUseCase],
+  controllers: [
+    RegisterParticipantController,
+    AuthenticateParticipantController,
+  ],
+  providers: [RegisterParticipantUseCase, AuthenticateParticipantUseCase],
 })
 export class HttpModule {}
