c4fd83702459e03d3f8fdc28995355beee98167d
Eduardo Tavares <eduardot.santosdev@gmail.com>
2024-12-22 19:59:49 -0300

    feat: adiciona o get participant by id

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,18 +1,30 @@
 import { Module } from '@nestjs/common'
 
 import { AuthenticateParticipantUseCase } from '@/domain/application/use-cases/authenticate-participant'
+// import { CreateSessionUseCase } from '@/domain/application/use-cases/create-session'
+import { GetParticipantByIdUseCase } from '@/domain/application/use-cases/get-participant-by-id'
 import { RegisterParticipantUseCase } from '@/domain/application/use-cases/register-participant'
+import { CacheModule } from '@/infra/cache/cache.module'
 import { CryptographyModule } from '@/infra/cryptography/cryptography.module'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { AuthenticateParticipantController } from '@/infra/http/controllers/authenticate-participant.controller'
+// import { CreateSessionController } from '@/infra/http/controllers/create-session.controller'
+import { GetParticipantByIdController } from '@/infra/http/controllers/get-participant-by-id.controller'
 import { RegisterParticipantController } from '@/infra/http/controllers/register-participant.controller'
 
 @Module({
-  imports: [DatabaseModule, CryptographyModule],
+  imports: [DatabaseModule, CryptographyModule, CacheModule],
   controllers: [
     RegisterParticipantController,
     AuthenticateParticipantController,
+    GetParticipantByIdController,
+    // CreateSessionController,
   ],
-  providers: [RegisterParticipantUseCase, AuthenticateParticipantUseCase],
+  providers: [
+    RegisterParticipantUseCase,
+    AuthenticateParticipantUseCase,
+    GetParticipantByIdUseCase,
+    // CreateSessionUseCase,
+  ],
 })
 export class HttpModule {}
