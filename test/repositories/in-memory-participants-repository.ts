e12068beb212ccb6c02295b15e66b69abc3d569b
Eduardo Tavares <eduardot.santosdev@gmail.com>
2024-12-22 22:31:38 -0300

    test: adiciona findById

--- test/repositories/in-memory-participants-repository.ts
+++ test/repositories/in-memory-participants-repository.ts
@@ -4,6 +4,16 @@ import { Participant } from '@/domain/enterprise/entities/participant'
 export class InMemoryParticipantsRepository implements ParticipantsRepository {
   public items: Participant[] = []
 
+  async findById(id: string) {
+    const participant = this.items.find((item) => item.id.toString() === id)
+
+    if (!participant) {
+      return null
+    }
+
+    return participant
+  }
+
   async findByEmail(email: string) {
     const participant = this.items.find((item) => item.email === email)
 
